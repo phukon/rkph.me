42873e37fd1a29da760cd8ffeda2f222e06d74aa
phukon <hello@rikiphukon.com>
2023-12-29 17:30:49 +0530

    feat(analytics): add PostHog

--- phukon-nextjs/app/layout.tsx
+++ phukon-nextjs/app/layout.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import clsx from "clsx";
+import { PHProvider, PostHogPageview } from "@/providers/PostHog";
+import { Suspense } from "react";
+
 
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Navigation from "@/components/Navigation";
@@ -22,28 +25,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        className={clsx(
-          inter.className,
-          "antialiased bg-primary text-primary width-full"
-        )}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
+      <Suspense>
+        <PostHogPageview />
+      </Suspense>
+      <PHProvider>
+        <body
+          className={clsx(
+            inter.className,
+            "width-full bg-primary text-primary antialiased",
+          )}
         >
-          <Navigation />
-          <div
-            className={
-              "px-6 md:px-6 pt-16 pb-24 md:pt-20 md:pb-44 max-w-[700px] mx-auto"
-            }
-          >
-            {children}
-          </div>
-        </ThemeProvider>
-        <Analytics />
-      </body>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <Navigation />
+            <div
+              className={
+                "mx-auto max-w-[700px] px-6 pb-24 pt-16 md:px-6 md:pb-44 md:pt-20"
+              }
+            >
+              {children}
+            </div>
+          </ThemeProvider>
+          <Analytics />
+        </body>
+      </PHProvider>
     </html>
   );
 }
